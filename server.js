4678c3d4742264c1a2e97c1a66b87de49b5880a4
Moshe Nehemiah <moshe.nehemiah@gmail.com>
2022-12-15 18:42:28 +0200

    fix cors

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ app.use(express.json())
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.resolve(__dirname, 'public')))
-    app.use(cors())
+    const corsOptions = {
+        origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
+        credentials: true
+    }
+    app.use(cors(corsOptions))
 } else {
     const corsOptions = {
         origin: ['http://127.0.0.1:5173', 'http://localhost:5173'],
