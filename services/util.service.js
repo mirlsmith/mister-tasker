67b5d543e0b58ac75306e45d070505d3e74359b3
Y-Moshe <moshe.nehemiah@gmail.com>
2022-12-16 14:27:55 +0200

    add makeLorem util

--- services/util.service.js
+++ services/util.service.js
@@ -61,11 +61,22 @@ function timeAgo(ms = new Date()) {
   }
 }
 
+function makeLorem(size = 100) {
+  var words = ['The sky', 'above', 'the port', 'was', 'the color of television', 'tuned', 'to', 'a dead channel', '.', 'All', 'this happened', 'more or less', '.', 'I', 'had', 'the story', 'bit by bit', 'from various people', 'and', 'as generally', 'happens', 'in such cases', 'each time', 'it', 'was', 'a different story', '.', 'It', 'was', 'a pleasure', 'to', 'burn']
+  var txt = ''
+  while (size > 0) {
+      size--
+      txt += words[Math.floor(Math.random() * words.length)] + ' '
+  }
+  return txt
+}
+
 module.exports = {
   makeId,
   getRandomInt,
   debounce,
   deepCopy,
   timeAgo,
-  generateRandomImg
+  generateRandomImg,
+  makeLorem
 }
